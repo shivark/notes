54aa98b5662698cce58746b32ba3d556c6f7e27e
Shiva Razzagh Karimi <shivarazzaghkarimi@shivas-mbp.home>
2018-12-31 18:40:16 +0000

    Refactored made it async

--- controllers/index.controller.mjs
+++ controllers/index.controller.mjs
@@ -2,12 +2,9 @@ import path from 'path';
 import Note from './../models/note.mjs';
 
 export function get(req, res) {
-    //res.render(path.resolve() + '/views/index', { data: getNotes() });
-    Note.find({}, function(err, docs) {
-        if (!err) {
-            res.render(path.resolve() + '/views/index', { data: docs });
-        }
-    });
+    getNotes().then(data => {
+        res.render(path.resolve() + '/views/index', ({ data }));
+    })
 }
 
 export function create(req, res) {
@@ -19,18 +16,14 @@ export function post(req, res) {
     var data = new Note(req.body);
 
     data.save()
-        .then(item => {
-            res.render(path.resolve() + '/views/index', { notes: getNotes() });
-        })
+        .then(() => getNotes().then(data => {
+            res.render(path.resolve() + '/views/index', ({ data }));
+        }))
         .catch(err => {
             res.status(400).send("unable to save to database");
         });
 }
 
 async function getNotes() {
-
-    // const notes = [{ name: 'first note', note: 'hello' }, { name: 'second note', note: 'hello world' }];
-    //return notes;
-
     return await Note.find({}).exec();
 }
